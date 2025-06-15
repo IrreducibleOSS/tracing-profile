5e976cf10c9d28a2a365d41b1278cc1d51413945
Sergei Shulepov <s.pepyakin@gmail.com>
2025-06-15 18:47:18 +0200

    emit max rss (#21)
    Unlike the [original version](https://app.graphite.dev/github/pr/IrreducibleOSS/binius/590/%5Bcore%5D-track-maxrss), this uses nix which was handy.

--- src/utils.rs
+++ src/utils.rs
@@ -85,3 +85,34 @@ pub fn get_git_info() -> Option<GitInfo> {
         is_clean,
     })
 }
+
+/// Creates a [`tracing`] event with the resident set size at its peak in megabytes.
+///
+/// The name of the event is `max rss mib`.
+pub fn emit_max_rss() {
+    if let Some(max_rss) = get_max_rss() {
+        let max_rss_mb = if cfg!(target_os = "linux") {
+            // The maxrss is in kbytes for Linux.
+            max_rss / 1024
+        } else if cfg!(target_os = "macos") {
+            // ... and in bytes for BSD/macOS.
+            max_rss / 1024 / 1024
+        } else {
+            // don't risk confusing.
+            0
+        };
+        tracing::event!(
+            name: "max rss mib",
+            tracing::Level::INFO,
+            value = max_rss_mb,
+            counter = true
+        );
+    }
+    fn get_max_rss() -> Option<i64> {
+        use nix::sys::resource;
+
+        resource::getrusage(resource::UsageWho::RUSAGE_SELF)
+            .ok()
+            .map(|usage| usage.max_rss())
+    }
+}
