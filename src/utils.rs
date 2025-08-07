a952984178a3c32b55aae8214d99fd30bf6b15a9
jimpo <881253+jimpo@users.noreply.github.com>
2025-08-07 08:53:53 +0000

    [chore] Remove unused function (#31)

--- src/utils.rs
+++ src/utils.rs
@@ -11,16 +11,6 @@ pub fn get_formatted_time() -> (String, String) {
     (now.format("%Y_%m_%d_%H_%M").to_string(), now.to_rfc3339())
 }
 
-pub fn get_current_branch_revision() -> Option<String> {
-    let repo = Repository::discover(".").ok()?;
-    let head = repo.head().ok()?;
-    let branch = head.shorthand()?;
-    let commit = head.peel_to_commit().ok()?;
-    let short_hash = &commit.id().to_string()[..7];
-
-    Some(format!("{}_{}", sanitize_filename(branch), short_hash))
-}
-
 pub fn sanitize_filename(branch: &str) -> String {
     branch
         .chars()
